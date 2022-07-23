5d8580e6e73b22cb340261ce0e0a497ca8fb91f1
Prashant bhatt <93563996+Prashant2242@users.noreply.github.com>
2022-07-24 00:21:09 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>This is a simple react todos list app that allows users to add, edit, and delete tasks they want to work on. </p>
+        </div>
+    )
+}
